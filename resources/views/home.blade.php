523c9052aef38436f742447b6d280e81b7df7e54
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-07-07 12:15:49 +0100

    add username in the frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
   <div class="row">
       <div class="col-3 p-5">
-            <img src="https://s.ftcdn.net/v2013/pics/all/curated/RKyaEDwp8J7JKeZWQPuOVWvkUjGQfpCx_cover_580.jpg?r=1a0fc22192d0c808b8bb2b9bcfbf4a45b1793687" alt="" class="rounded-circle" style="height: 50px">
+            <img src="https://s.ftcdn.net/v2013/pics/all/curated/RKyaEDwp8J7JKeZWQPuOVWvkUjGQfpCx_cover_580.jpg?r=1a0fc22192d0c808b8bb2b9bcfbf4a45b1793687" alt="" class="rounded-circle" style="height: 100px; width: 100px">
       </div>
       <div class="vol-9">
 
